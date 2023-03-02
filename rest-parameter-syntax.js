8a619367972cf4fd36b54f2d0972cefa76e4e326
tWilltp <williamjenkinsfeb@gmail.com>
2023-03-02 15:30:24 +0000

    rest perameter syntax

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -5,6 +5,24 @@
 
 // Regular function call 
 
+const sumAll = (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log('sum 1:', sum);
+
 // Extra arguments are ignored
 
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log('sum 2:', sum2);
+
 // Function using ...rest
+
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i;
+    }
+    return sum
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log('sum 3:', sum3);
